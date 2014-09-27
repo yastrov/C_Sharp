275d72e2cea364d89edbbf5856a7853f1c1289d6
Yuri Astrov <yuriastrov@gmail.com>
2014-09-27 17:14:47 +0400

    Genric test added.

---
+++ Core/GenericTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenricTest
+{
+    public interface IA<T>
+    {
+        T Value { get; set; }
+    }
+
+    public interface IB<T> : IA<T>
+    {
+        T Foo();
+    }
+
+    public class  MyClass<T>: IB<T>
+    {
+        private T _value;
+        public T Value {
+            get { return this._value; }
+            set { this._value = value; }
+        }
+        public MyClass(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Foo()
+        {
+            return this._value;
+        }
+    }
+
+    class Program
+    {
+        // Type with parametherless constructor
+        // static T Max <T>(T a, T b) where T : new()
+        static T Max <T>(T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Max<int>(1,4));
+            Console.WriteLine(Max(1, 4));
+            Console.WriteLine(new MyClass<int>(5).Foo());
+        }
+    }
+}
