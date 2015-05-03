9fcdfe74f36db2eb7f4ce18de8da510c7d54ebfd
Yuriy Astrov <yuriastrov@gmail.com>
2015-05-03 12:23:21 +0300

    MissAndCan: Cancellation added.

--- Algorithms/MissAndCan/MissAndCan/Algorithm.cs
+++ Algorithms/MissAndCan/MissAndCan/Algorithm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MissAndCan
@@ -36,6 +37,39 @@ namespace MissAndCan
                         fringe.Add(node);
                     }
                 }
+            }
+            return result;
+        }
+
+        public static State UniformCostSearch(Problem problem, CancellationToken cancelToken)
+        {
+            State result = null;
+            var comparer = problem.GetComparer();
+            var visited = new HashSet<State>(comparer);
+            // It's need other collection as it possible! Red Black Tree for example.
+            List<State> fringe = new List<State>();
+            fringe.Add(problem.GetInitialState());
+            while (fringe.Count != 0)
+            {
+                // If we use heuristic function with problem.GetCostOfActions(q), we can take A* algorythm:).
+                // It's need other collection as it possible!
+                var currNode = fringe.OrderBy(q => problem.GetCostOfActions(q)).First();
+                fringe.Remove(currNode);
+                if (problem.IsGoal(currNode))
+                {
+                    result = currNode;
+                    break;
+                }
+                if (!visited.Contains(currNode))
+                {
+                    visited.Add(currNode);
+                    foreach (var node in problem.GetSuccessors(currNode))
+                    {
+                        // It's not good idea, but I have no more best solution on this time!
+                        fringe.Add(node);
+                    }
+                }
+                cancelToken.ThrowIfCancellationRequested();
             }
             return result;
         }
--- Algorithms/MissAndCan/MissAndCan/MainWindow.xaml.cs
+++ Algorithms/MissAndCan/MissAndCan/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -53,6 +54,7 @@ namespace MissAndCan
                 NotifyPropertyChanged("Combinations");
             }
         }
+        private CancellationTokenSource cancelSource = null;
 
         public MainWindow()
         {
@@ -61,15 +63,20 @@ namespace MissAndCan
 
         async private void StartButtoCllick(object sender, RoutedEventArgs e)
         {
+            cancelSource = new CancellationTokenSource();
             Cursor = Cursors.Wait;
             IsEnabled = false;
             try
             {
-                var result = await MakeResult();
+                var result = await MakeResult(cancelSource.Token);
                 if (result != null && result.Count != 0)
                     Combinations = new ObservableCollection<StateView>(result);
                 else
                     MessageBox.Show("No solution!", Title, MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch(OperationCanceledException ex)
+            {
+                MessageBox.Show("Cancelled by user!", Title, MessageBoxButton.OK, MessageBoxImage.Asterisk);
             }
             finally
             {
@@ -78,14 +85,14 @@ namespace MissAndCan
             }
         }
 
-        async private Task<List<StateView>> MakeResult()
+        async private Task<List<StateView>> MakeResult(CancellationToken cancelToken)
         {
             return await Task.Run(() =>
             {
                 List<StateView> resultCollection = null;
                 var p = new Problem(new State(3, 3, BoatState.Left, 0),
-                new State(0, 0, BoatState.Right, 0));
-                var result = Algorithm.UniformCostSearch(p);
+                    new State(0, 0, BoatState.Right, 0));
+                var result = Algorithm.UniformCostSearch(p, cancelToken);
                 if (result != null)
                 {
                     Stack<State> stack = new Stack<State>();
@@ -94,6 +101,7 @@ namespace MissAndCan
                         stack.Push(result);
                         result = result.PrevState;
                     }
+                    cancelToken.ThrowIfCancellationRequested();
                     resultCollection = new List<StateView>(stack.Count);
                     int n = 0;
                     foreach (var state in stack)
@@ -103,9 +111,31 @@ namespace MissAndCan
                     return resultCollection;
                 }
                 return new List<StateView>();
-            });
+            }, cancelToken);
         }
 
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            if (cancelSource != null)
+            {
+                try
+                {
+                    cancelSource.Cancel();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), Title, MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+        }
+
+        private void CancelButtoCllick(object sender, RoutedEventArgs e)
+        {
+            if (cancelSource != null)
+            {
+                cancelSource.Cancel();
+            }
+        }
 
     }
 }
